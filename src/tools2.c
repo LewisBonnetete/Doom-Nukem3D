c6c69e9dc991e7bffdbcd85095a51350a7c9f6d9
Tom Rabut <tomrabut1@gmail.com>
2020-06-05 11:13:28 +0200

    asd

--- src/tools2.c
+++ src/tools2.c
@@ -21,3 +21,55 @@ int	do_trigo(t_var *info, t_wall *wall)
 	}
 	return (0);
 }
+
+// int		hitboxx(t_var *info, double dirx)
+// {
+// 	double x;
+// 	double y;
+
+// 	x = -0.2;
+// 	y = -0.2;
+// 	while (x < 0.2)
+// 	{
+// 		y = -0.2;
+// 		while (y < 0.2)
+// 		{
+// 			if (x * x + y * y  <= 0.17 * 0.17 && info->map[(int)(info->posx + x)][(int)(info->posy)] == '#')
+// 			{
+// 				if(dirx >= 0 && x >= 0)
+// 					return (0);
+// 				if(dirx <= 0 && x <= 0)
+// 					return (0);
+// 			}
+// 			y += 0.01;
+// 		}
+// 		x += 0.01;
+// 	}
+// 	return(1);
+// }
+
+// int		hitboxy(t_var *info, double diry)
+// {
+// 	double x;
+// 	double y;
+
+// 	x = -0.2;
+// 	y = -0.2;
+// 	while (x < 0.2)
+// 	{
+// 		y = -0.2;
+// 		while (y < 0.2)
+// 		{
+// 			if (x * x + y * y  <= 0.17 * 0.17 && info->map[(int)(info->posx)][(int)(info->posy + y)] == '#')
+// 			{
+// 				if(diry >= 0 && y >= 0)
+// 					return (0);
+// 				if(diry <= 0 && y <= 0)
+// 					return (0);
+// 			}
+// 			y += 0.01;
+// 		}
+// 		x += 0.01;
+// 	}
+// 	return(1);
+// }
