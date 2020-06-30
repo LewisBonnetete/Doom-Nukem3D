e50a474e70cfed6470734f636cf8f4622b02e1ae
Tom RABUT <trabut@e1r5p6.42.fr>
2020-06-30 15:57:16 +0200

    a bit more optimal drawing

--- src/algo_draw.c
+++ src/algo_draw.c
@@ -89,18 +89,18 @@ static	void	ft_put_weapon(t_var *info, t_render *render)
 	Uint32	color;
 
 	x = 0;
-	while (x < render->tab_sdl[3]->w * 2)
+	while (x < render->tab_sdl[3]->w)
 	{
 		y = 0;
-		while (y < render->tab_sdl[3]->h * 2)
+		while (y < render->tab_sdl[3]->h)
 		{
-			color = get_pixel(render->tab_sdl[3], (int)y, (int)x);
+			color = get_pixel(render->tab_sdl[3], y, x);
 			if (color != 0)
 				put_pixel_to_suface(color, (int)x + WINDOW_W / 2 - 45,
 				WINDOW_H + (int)y - 125 + info->d_gun, info->image);
-			y += 0.5;
+			y++;
 		}
-		x += 0.5;
+		x++;
 	}
 }
 
