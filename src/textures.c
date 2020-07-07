f31fac05038f69588ea804b233c8228729ba1a2b
Alexis TYCZYNSKI <atyczyns@e1r3p2.42.fr>
2020-07-07 16:56:53 +0200

    protect

--- src/textures.c
+++ src/textures.c
@@ -1,6 +1,6 @@
 #include "doom_nukem.h"
 
-void	tab_path(t_render *renderer)
+int		tab_path(t_render *renderer)
 {
 	int	i;
 
@@ -9,21 +9,22 @@ void	tab_path(t_render *renderer)
 	{
 		if (i == 0)
 			if ((renderer->tab_sdl[i] = IMG_Load("./xpm_textures/stone2.png")) == 0)
-				return;
+				return (0);
 		if (i == 1)
 			if ((renderer->tab_sdl[i] = IMG_Load("./xpm_textures/dark_wall.png")) == 0)
-				return;
+				return (0);
 		if (i == 2)
 			if ((renderer->tab_sdl[i] = IMG_Load("./xpm_textures/stone3.png")) == 0)
-				return;
+				return (0);
 		if (i == 3)
 			if ((renderer->tab_sdl[i] = IMG_Load("./xpm_textures/Jagpistol.png")) == 0)
-				return;
+				return (0);
 	}
 	renderer->tab_sdl[i] = NULL;
+	return (1);
 }
 
-void	tab_path_text(t_render *renderer)
+int		tab_path_text(t_render *renderer)
 {
 	int	i;
 
@@ -32,27 +33,28 @@ void	tab_path_text(t_render *renderer)
 	{
 		if (i == 0)
 			if ((renderer->tab_sdl_item[i] = IMG_Load("./xpm_textures/ak47.png")) == 0)
-				return;
+				return (0);
 		if (i == 1)
 			if ((renderer->tab_sdl_item[i] = IMG_Load("./xpm_textures/creeper.png")) == 0)
-				return;
+				return (0);
 		if (i == 2)
 			if ((renderer->tab_sdl_item[i] = IMG_Load("./xpm_textures/barrel.png")) == 0)
-				return;
+				return (0);
 		if (i == 3)
 			if ((renderer->tab_sdl_item[i] = IMG_Load("./xpm_textures/soldier.png")) == 0)
-				return;
+				return (0);
 		if (i == 4)
 			if ((renderer->tab_sdl_item[i] = IMG_Load("./xpm_textures/heal.png")) == 0)
-				return;
+				return (0);
 		if (i == 5)
 			if ((renderer->tab_sdl_item[i] = IMG_Load("./xpm_textures/balle.png")) == 0)
-				return;
+				return (0);
 		if (i == 6)
 			if ((renderer->tab_sdl_item[i] = IMG_Load("./xpm_textures/dos_soldier.png")) == 0)
-				return;
+				return (0);
 	}
 	renderer->tab_sdl_item[7] = NULL;
+	return (1);
 }
 // int	load_text(char **tab, t_render renderer)
 // {
