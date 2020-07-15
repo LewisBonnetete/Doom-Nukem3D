85ecdd579e0a4322fbf269b0c1ca118233f1e569
Lewis BONNETETE <lbonnete@e1r5p4.42.fr>
2020-07-15 17:32:11 +0200

    idk

--- src/game_init.c
+++ src/game_init.c
@@ -6,7 +6,7 @@
 /*   By: lbonnete <lbonnete@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 17:04:28 by lewis             #+#    #+#             */
-/*   Updated: 2020/07/09 18:28:04 by lbonnete         ###   ########.fr       */
+/*   Updated: 2020/07/15 16:55:56 by lbonnete         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ void			init_player(t_player *player, t_map *map)
 	player->hp = 100;
 	player->hp_p = 100;
 	player->ammo = 0;
+	player->no_scopex = 0;
+	player->no_scopey = 0;
 }
 
 void			init_render(t_var *info, t_render *render,
